9a336410ac9656e1c46c8a59b76bf7a4cf6fe751
toshimickey <toshi123ut@icloud.com>
2023-10-30 20:11:57 +0900

    update vae imgsz

--- utils/autoencoder.py
+++ utils/autoencoder.py
@@ -8,29 +8,32 @@ class VAE(nn.Module):
 
         # エンコーダー
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=4, stride=2, padding=1),
+            # 3,32,32→8,16,16
+            nn.Conv2d(3, 8, kernel_size=4, stride=2, padding=1),
             nn.ReLU(True),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
+            # 8,16,16→16,8,8
+            nn.Conv2d(8, 16, kernel_size=4, stride=2, padding=1),
             nn.ReLU(True),
-            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            # 16,8,8→32,4,4
+            nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=1),
             nn.ReLU(True)
         )
         
-        self.fc_mu = nn.Linear(128 * 32 * 32, latent_dim)
-        self.fc_logvar = nn.Linear(128 * 32 * 32, latent_dim)
+        self.fc_mu = nn.Linear(32 * 4 * 4, latent_dim)
+        self.fc_logvar = nn.Linear(32 * 4 * 4, latent_dim)
 
         self.decoder1 = nn.Sequential(
-            nn.Linear(latent_dim, 128 * 32 * 32),
+            nn.Linear(latent_dim, 32 * 4 * 4),
             nn.ReLU(True)
         )
 
         # デコーダー
         self.decoder2 = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),
             nn.ReLU(True),
-            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(16, 8, kernel_size=4, stride=2, padding=1),
             nn.ReLU(True),
-            nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(8, 3, kernel_size=4, stride=2, padding=1),
             nn.Sigmoid()
         )
 
@@ -50,7 +53,7 @@ class VAE(nn.Module):
         z = self.reparameterize(mu, logvar)
 
         x = self.decoder1(z)
-        x = x.view(x.size(0), 128, 32, 32)
+        x = x.view(x.size(0), 32, 4, 4)
         x = self.decoder2(x)
         return x, mu, logvar, z
 
